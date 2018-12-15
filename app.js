36be42e31497998ae256d475c985288278ff2e55
Garig <alexandre.houriez@yahoo.fr>
2018-12-15 11:59:40 +0100

    cinquieme commit

--- app.js
+++ app.js
@@ -9,7 +9,8 @@ const port = process.env.PORT || 3000;
 const hostname = "localhost";
 
 // Database
-mongoose.connect('mongodb://localhost/todoapp', {useNewUrlParser: true}).then(() => {
+// mongoose.connect('mongodb://localhost/todoapp', {useNewUrlParser: true}).then(() => {
+mongoose.connect('mongodb://Garig:indiana31@@ds129904.mlab.com:29904/garig', {useNewUrlParser: true}).then(() => {
     console.log('Connecté à la base de données')
 });
 
