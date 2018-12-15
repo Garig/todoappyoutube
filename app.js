01c398e3dfa83aca7265a6a62ae03b1bec289bd8
Garig <alexandre.houriez@yahoo.fr>
2018-12-15 12:13:21 +0100

    sixieme commit

--- app.js
+++ app.js
@@ -10,9 +10,10 @@ const hostname = "localhost";
 
 // Database
 // mongoose.connect('mongodb://localhost/todoapp', {useNewUrlParser: true}).then(() => {
-mongoose.connect('mongodb://Garig:indiana31@@ds129904.mlab.com:29904/garig', {useNewUrlParser: true}).then(() => {
-    console.log('Connecté à la base de données')
-});
+mongoose.connect('mongodb://Garig:indiana31@@ds129904.mlab.com:29904/garig', {useNewUrlParser: true})
+// .then(() => {
+//     console.log('Connecté à la base de données')
+// });
 
 // Template engine
 app.set('view engine', 'pug')
