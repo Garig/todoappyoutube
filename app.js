2b7dde3c0bf5b9217d1cc67b12a35473ae9f5923
Garig <alexandre.houriez@yahoo.fr>
2018-12-15 12:52:51 +0100

    12 commit

--- app.js
+++ app.js
@@ -10,13 +10,23 @@ const port = process.env.PORT || 3000;
 
 // Database
 // mongoose.connect('mongodb://localhost/todoapp', {useNewUrlParser: true}).then(() => {
-mongoose.connect('mongodb://Garig:indiana31@ds129904.mlab.com:29904/garig');
+// mongoose.connect('mongodb://Garig:indiana31@ds129904.mlab.com:29904/garig', {useNewUrlParser: true});
 // .then(() => {
 //     console.log('Connecté à la base de données')
 // });
 
+var options = { server: { socketOptions: { keepAlive: 300000, connectTimeoutMS: 30000 } }, 
+replset: { socketOptions: { keepAlive: 300000, connectTimeoutMS : 30000 } } };
+var urlmongo = "mongodb://Garig:indiana31@ds129904.mlab.com:29904/garig"; 
+mongoose.connect(urlmongo, options);
+var db = mongoose.connection; 
+db.on('error', console.error.bind(console, 'Erreur lors de la connexion')); 
+db.once('open', function (){
+    console.log("Connexion à la base OK"); 
+}); 
+
 // Template engine
-app.set('view engine', 'pug');
+app.set('view engine', 'pug')
 
 // Body parser
 app.use(bodyParser.urlencoded({ extended: false }));
